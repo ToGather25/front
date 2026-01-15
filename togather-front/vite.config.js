f197ac24f9c33c93d4a4ea2b041f7e9dfe090002
yaewon <yw.lim@quintet.co.kr>
2026-01-15 13:27:25 +0900

    design: logo 변경

--- togather-front/vite.config.js
+++ togather-front/vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '192x192.png',
+            src: 'icons/192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '512x512.png',
+            src: 'icons/512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
