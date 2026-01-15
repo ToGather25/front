6afd65e700b6837b59bab6edc4aa5470db1beea2
yaewon <yw.lim@quintet.co.kr>
2026-01-15 09:38:14 +0900

    feat: 프로젝트 생성 및 PWA 적용

---
+++ togather-front/vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'ToGather',
+        short_name: 'ToGather',
+        description: '함께 모여 하나님 아버지께로, ToGather',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
